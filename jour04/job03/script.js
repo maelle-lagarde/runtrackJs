a931a2751765ea55d08bfcd183af4288ae452547
Maëlle Lagarde <maelle.lagarde@MacBook-Pro-de-Maelle.local>
2023-07-04 14:16:12 +0200

    mardi 4 juillet

--- jour04/job03/script.js
+++ jour04/job03/script.js
@@ -1,43 +1,49 @@
-$(document).ready(function () {
-    function filterData() {
-        let id = document.getElementById('id').value;
-        let name = document.getElementById('name').value;
-        let type = document.getElementById('type').value;
+function filterData() {
+    // Obtention des valeurs des champs de formulaire
+    const id = document.getElementById('id').value;
+    const name = document.getElementById('name').value;
+    const type = document.getElementById('type').value;
 
-        const url = './pokemon.json';
+    // Construction de l'URL pour récupérer le fichier JSON
+    const url = './pokemon.json';
 
-        fetch(url)
-            .then(response =>response.json())
-            .then(data => {
-                const filteredData = data.filter(pokemon => {
-                    if (id && pokemon.id !== id) {
-                        return false;
-                    }
-                    if (name && pokemon.name !== name) {
-                        return false;
-                    }
-                    if (type && pokemon.type !== type) {
-                        return false;
-                    }
-                    return true;
-                });
-                displayResults(filteredData);
-            })
-            .catch(error => console.error('Erreur lors de la récupération des données :', error));
-    }
+    // Utilisation de l'API Fetch pour récupérer les données du fichier JSON
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            // Filtrage des données en fonction des critères sélectionnés
+            const filteredData = data.filter(pokemon => {
+                if (id && pokemon.id !== parseInt(id)) {
+                    return false;
+                }
+                if (name && pokemon.name.french.toLowerCase() !== name.toLowerCase()) {
+                    return false;
+                }
+                if (type && !pokemon.type.includes(type)) {
+                    return false;
+                }
+                return true;
+            });
 
-    function displayResults(data) {
-        const resultsDiv = document.getElementById('results');
-
-        resultsDiv.innerHTML = '';
-
-        data.forEach(pokemon => {
-            const pokemonDiv = document.createElement('div');
-            pokemonDiv.innerHTML = `<p>ID: ${pokemon.id}</p>
-                                <p>Nom: ${pokemon.name}</p>
-                                <p>Type: ${pokemon.type}</p>
-                                <hr>`;
-            resultsDiv.appendChild(pokemonDiv);
+            // Affichage des résultats sur la page HTML
+            displayResults(filteredData);
         })
-    }
-})
+        .catch(error => console.error('Erreur lors de la récupération des données:', error));
+}
+
+function displayResults(data) {
+    const resultsDiv = document.getElementById('results');
+
+    // Effacer les résultats précédents
+    resultsDiv.innerHTML = '';
+
+    // Création des éléments HTML pour chaque Pokémon filtré
+    data.forEach(pokemon => {
+        const pokemonDiv = document.createElement('div');
+        pokemonDiv.innerHTML = `<p>ID: ${pokemon.id}</p>
+                                <p>Nom: ${pokemon.name.french}</p>
+                                <p>Type: ${pokemon.type.join(', ')}</p>
+                                <hr>`;
+        resultsDiv.appendChild(pokemonDiv);
+    });
+}
